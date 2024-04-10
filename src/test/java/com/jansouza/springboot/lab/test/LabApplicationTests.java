65a7fe74d997a6558cb697e2f22d48a2d33f8990
Jan Souza <jansouza@users.noreply.github.com>
2024-04-10 11:51:30 -0300

    Fix compile issue

--- src/test/java/com/jansouza/springboot/lab/test/LabApplicationTests.java
+++ src/test/java/com/jansouza/springboot/lab/test/LabApplicationTests.java
@@ -1,12 +1,13 @@
 package com.jansouza.springboot.lab.test;
 
+import com.jansouza.springboot.lab.GreetingController;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 import org.junit.jupiter.api.Test;
 
