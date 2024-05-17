2e45fe21d3ce0cf08b9e9da0a9804234b630d106
Jan Souza <jansouza@users.noreply.github.com>
2024-05-17 15:53:07 -0300

    Add logback

--- src/main/java/com/jansouza/springboot/lab/GreetingController.java
+++ src/main/java/com/jansouza/springboot/lab/GreetingController.java
@@ -1,5 +1,8 @@
 package com.jansouza.springboot.lab;
 
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+	private static final Logger logger = LoggerFactory.getLogger(GreetingController.class.getName());
+
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="greeting") String name, Model model) {
 		model.addAttribute("name", name);
+
+		logger.debug("GreetingController");
+		logger.trace("{}",new JSONObject().put("action", "greeting").put("response", 200));
+		
 		return "greeting";
 	}
 
